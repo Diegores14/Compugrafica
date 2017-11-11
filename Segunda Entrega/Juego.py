4c4bd1ba09e751cc1eede3b69c3f616bff728c54
Diegores14 <diegorestrepo68@utp.edu.co>
2017-11-11 00:08:54 -0500

    implementacion de grupos y sprites

--- Segunda Entrega/Juego.py
+++ Segunda Entrega/Juego.py
@@ -1,5 +1,5 @@
 import pygame
-import ConfigParser
+import configparser
 
 ALTO=500
 ANCHO=700
@@ -15,22 +15,90 @@ class Interfaz():
 					pantalla.blit(corte,pos)
 		pygame.display.flip()
 
-class Jugador():
+class Arbol(pygame.sprite.Sprite):
+	def __init__(self, x, y):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.image.load('Arboles2.png').convert_alpha()
+		self.rect=self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
+
+	def update(self):
+		pass
+
+
+class Casa(pygame.sprite.Sprite):
 	def __init__(self):
+		pygame.sprite.Sprite.__init__(self)
 		self.imagen = pygame.image.load('pokemon2.png').convert_alpha()
-		self.jugador = imagen.subsurface((1555,1744, 106, 82))
+		self.image = self.imagen.subsurface((1555,1744, 106, 82))
+		self.rect=self.image.get_rect()
+
+class Agua(pygame.sprite.Sprite):
+	def __init__(self):
+		pygame.sprite.Sprite.__init__(self)
+		self.imagen = pygame.image.load('pokemon2.png').convert_alpha()
+		self.image = self.imagen.subsurface((1555,1744, 106, 82))
+		self.rect=self.image.get_rect()
+
+class Bloque(pygame.sprite.Sprite):
+	def __init__(self):
+		pygame.sprite.Sprite.__init__(self)
+		self.imagen = pygame.image.load('pokemon2.png').convert_alpha()
+		self.image = self.imagen.subsurface((1555,1744, 106, 82))
+		self.rect=self.image.get_rect()
 
 
+class Jugador(pygame.sprite.Sprite):
+	def __init__(self):
+		pygame.sprite.Sprite.__init__(self)
+		self.imagen = pygame.image.load('pokemon2.png').convert_alpha()
+		self.jugador = self.imagen.subsurface((1555,1744, 106, 82))
+		self.dir = 0
+		self.x = 0
+		self.var_x = 0
+		self.var_y = 0
+		self.m = []
+		for i in range(4):
+			aux = []
+			for j in range(3):
+				aux.append(self.jugador.subsurface((i*30, j*30, 16, 22)))
+			self.m.append(aux)
+		self.image = self.m[self.dir][self.x]
+		self.rect=self.image.get_rect()
+
+	def update(self):
+		self.rect.x+=self.var_x
+		self.rect.y+=self.var_y
+		if self.rect.x>=ANCHO-self.rect[2]:
+			self.var_x=ANCHO-self.rect[2]
+		if self.rect.x<=0:
+			self.rect.x=0
+			self.var_x=0
+		if self.rect.y>=ALTO-self.rect[3]:
+			self.var_y=ALTO-self.rect[3]
+		if self.rect.y<=0:
+			self.var_y=0
+		self.image=self.m[self.dir][self.x]
 
 if __name__=='__main__':
 	pygame.init()
 	pantalla=pygame.display.set_mode([ANCHO,ALTO])
-	interprete=ConfigParser.ConfigParser()
+	interprete=configparser.ConfigParser()
 	interprete.read('Mapa.map')
 	pantalla.fill((0,160,0))
-	j = Jugador()
-	V = Interfaz(pantalla, 'nivel1', interprete)
-	#pantalla.blit(jugador.subsurface((90, 30, 16, 22)), (0,0))
+	jp = Jugador()
+	# V = Interfaz(pantalla, 'nivel1', interprete)
+
+	general=pygame.sprite.Group()
+	general.add(jp)
+	objetos = pygame.sprite.Group()
+	ar = Arbol(0,0)
+	ar1 = Arbol(490,0)
+	objetos.add(ar)
+	general.add(ar)
+	objetos.add(ar1)
+	general.add(ar1)
 	reloj=pygame.time.Clock()
 	val = True
 	while val:
@@ -39,3 +107,5 @@ if __name__=='__main__':
 				val=False
 		pygame.display.flip()
 		reloj.tick(60)
+		general.draw(pantalla)
+		pygame.display.flip()
